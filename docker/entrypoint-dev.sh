dc5a8b88c84c1c7d4c0d9e4fed6d26f7505060b7
pcmagas <pcmagas@disroot.org>
2024-02-23 22:08:16 +0200

    Clean Docker environment.

--- docker/entrypoint-dev.sh
+++ docker/entrypoint-dev.sh
@@ -3,14 +3,11 @@
 usermod -u ${APP_UID} manipulator
 groupmod -g ${APP_GID} manipulator
 
-chown -R manipulator:manipulator /opt/manipulator
 chown manipulator:manipulator /var/log/manipulator
 
-if [ ! -d "/opt/manipulator/venv" ]; then
+if [ ! -d "/home/manipulator/venv" ]; then
     # Create virtual environment
-    su -c "python -m venv /opt/manipulator/venv" manipulator
+    su -c "python -m venv /home/manipulator/venv" manipulator
 fi
 
-su -c "source /opt/manipulator/venv/bin/activate" manipulator
-su -c "pip install -r requirements.txt" manipulator
-su -c "python /opt/manipulator/src/main.py" manipulator
+su -c "source /home/manipulator/venv/bin/activate && pip install -r requirements.txt &&  python /home/manipulator/app/src/main.py" manipulator
