74ec2b141b85a0d5626488a74fa50a187573fbf4
pcmagas <pcmagas@disroot.org>
2024-02-23 22:01:11 +0200

    Enable Virtualenv

--- docker/entrypoint-dev.sh
+++ docker/entrypoint-dev.sh
@@ -6,5 +6,11 @@ groupmod -g ${APP_GID} manipulator
 chown -R manipulator:manipulator /opt/manipulator
 chown manipulator:manipulator /var/log/manipulator
 
+if [ ! -d "/opt/manipulator/venv" ]; then
+    # Create virtual environment
+    su -c "python -m venv /opt/manipulator/venv" manipulator
+fi
+
+su -c "source /opt/manipulator/venv/bin/activate" manipulator
 su -c "pip install -r requirements.txt" manipulator
 su -c "python /opt/manipulator/src/main.py" manipulator
