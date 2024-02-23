388f028cf25f18a587c3458f1f50fbbffe8ef26c
pcmagas <pcmagas@disroot.org>
2024-02-23 23:38:44 +0200

    Skip execution if already executed.

--- src/services/db/migrations.py
+++ src/services/db/migrations.py
@@ -1,9 +1,8 @@
-import sqlite3
 import pathlib
 import re
 import os
 
-migrations_path = os.path.join(pathlib.Path(__file__).parent.absolute(),"../database/migrations")
+migrations_path = os.path.join(pathlib.Path(__file__).parent.absolute(),"../../database/migrations")
 
 def extract_first_digit(filename):
     match = re.search(r'^\d+', filename)
@@ -13,18 +12,21 @@ def get_file_extension(filename):
     # Split the base name and get the file extension
     _, file_extension = os.path.splitext(filename)
     
-    return file_extension
+    return file_extension.replace(".","")
 
 def execureSqlFile(conn,sqlFile):
     cursor = conn.cursor()
-
+    
     print("Run Sql migration file "+sqlFile)
+    
+    full_sqlFile = os.path.join(migrations_path,sqlFile)
 
-    with open(sqlFile, 'r') as script_file:
+    with open(full_sqlFile, 'r') as script_file:
         sql_script = script_file.read()
         try:
             conn.execute('BEGIN')
             cursor.executescript(sql_script)
+            cursor.execute("INSERT INTO migrations(migration_filename) VALUES (?)",[(sqlFile)])
             conn.commit()
         except Exception as e:
             conn.rollback()
@@ -36,28 +38,47 @@ def executeSqlInPythonScript(conn,pythonScript):
     print("execute "+pythonScript)
     return
 
+
+def createMigrationsTable(conn):
+    mig_table_sql="""
+    CREATE TABLE IF NOT EXISTS migrations (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        migration_filename TEXT NOT NULL
+    );
+"""
+    try:
+        cursor = conn.cursor()
+        conn.execute('BEGIN')
+        cursor.execute(mig_table_sql)
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        raise
+    finally:
+        cursor.close()
+
 def apply_migrations(conn):
     files = os.listdir(migrations_path)
     sorted_files = sorted(files, key=extract_first_digit)
 
-    cursor = conn.cursor()
+    createMigrationsTable(conn)
+    
+    sql = "SELECT * from migrations where migration_filename = ? LIMIT 1"
+    cur = conn.cursor()
 
     for filename in sorted_files:
+        
+        res = cur.execute(sql,[(filename)])
+        filename_in_db = res.fetchone()
+
+        if filename_in_db is not None:
+            print("Skipping Execution of "+filename)
+            continue
+
         ext = get_file_extension(filename)
-        ext = ext.replace(".","")
-        filename = os.path.join(migrations_path,filename)
         if ext == 'sql':
             execureSqlFile(conn,filename)
         elif ext == "py":
             executeSqlInPythonScript(conn,filename)
 
-def create_db(conn_url):
-
-    print("Connecting "+conn_url)
-    conn = sqlite3.connect(conn_url)
-    print("Setting Up DB")
-    apply_migrations(conn)
-    
-    return conn
-
-create_db
+apply_migrations
