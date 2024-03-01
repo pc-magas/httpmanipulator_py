e37c5800a87bb3d11f5b2ed119f4c0d4dfa64e35
pcmagas <pcmagas@disroot.org>
2024-03-01 19:20:34 +0200

    Verify Certificate

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import multiprocessing
-from manipulator.http_socket_server import SocketServer,TLSSocketServer
+from manipulator.http_socket_server import SocketServer
 from services.db import create_db
 
 if __name__ == "__main__":
@@ -15,12 +15,12 @@ if __name__ == "__main__":
     db_conn = create_db("/home/manipulator/db/app.db")
 
     
-    server = SocketServer(host, port, max_threads)
+    server = SocketServer(host, port, max_threads,False)
     server_process = multiprocessing.Process(target=server.start)
     server_process.start()
 
     # Add other main application code here if needed
-    tls_server = TLSSocketServer(host, tls_port, max_threads)
+    tls_server = SocketServer(host, tls_port, max_threads,True)
     tls_server_process = multiprocessing.Process(target=tls_server.start)
     tls_server_process.start()
 
--- src/manipulator/http_socket_server.py
+++ src/manipulator/http_socket_server.py
@@ -4,38 +4,58 @@ import queue
 import time
 import ssl
 
+import os,pathlib
+
+path = os.path.join(pathlib.Path(__file__).parent.absolute(),"../../certs")
+certfile = os.path.join(path,"cert.crt")
+keyfile = os.path.join(path,"key.key")
+
+print(path,certfile,keyfile)
+
 class SocketServer:
     """
         Basic Socket Server in python
     """
 
-    def __init__(self,host,port,max_threads):
-        print("Create Server For Http")
+    def __init__(self,host,port,max_threads,tls=False):
+        print("Create Server For Http")        
+
         self.host = host
         self.port = port
         self.server_socket = self.initSocket()
         self.max_threads = max_threads
-        self.request_queue = queue.Queue()        
+        self.request_queue = queue.Queue()   
+
+        self.ssl_context=None
+        if(tls == True):
+            print("Initialise SSL context")        
+            self.ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+            self.ssl_context.load_cert_chain(certfile=certfile, keyfile=keyfile)
 
     def initSocket(self):
         return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def postProcessSocket(self,client_socket):
-        return client_socket
-
+   
     def __accept(self):
         self.server_socket.listen(5)
         while True:
-            client_socket, client_address = self.server_socket.accept()
-            actual_client_socket = self.postProcessSocket(client_socket)
-            self.request_queue.put((actual_client_socket, client_address))
+            try:
+                client_socket, client_address = self.server_socket.accept()
+                
+                if self.ssl_context is not None :
+                    print(self.ssl_context)
+                    client_socket = self.ssl_context.wrap_socket(client_socket, server_side=True)
+
+                self.request_queue.put((client_socket, client_address))
+            except:
+                print("Error Occured")
 
 
     def __handle(self):
         while True:
-            # Dequeue a request and process it
-            client_socket, address = self.request_queue.get()    
-            print(address)
+            client_socket, address = self.request_queue.get()
+            print("Address",address)
+                
             # Read HTTP Request
             # Log Http Request
             # Manipulate Http Request
@@ -44,6 +64,8 @@ class SocketServer:
             content = '<html><body>Hello World</body></html>\r\n'.encode()
             headers = f'HTTP/1.1 200 OK\r\nContent-Length: {len(content)}\r\nContent-Type: text/html\r\n\r\n'.encode()
             client_socket.sendall(headers + content)
+          
+            client_socket.shutdown(socket.SHUT_RDWR)
             client_socket.close()
             self.request_queue.task_done()
 
@@ -58,11 +80,9 @@ class SocketServer:
         self.__initThreads()
         self.__accept()
 
-
-class TLSSocketServer(SocketServer):
-    def postProcessSocket(self,client_socket):
-        print("Load TLS")
-        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-        context.load_cert_chain(certfile='/etc/manipulator/certs/cert.crt', keyfile='/etc/manipulator/certs/key.key')
-        return context.wrap_socket(client_socket, server_side=True)
-
+if __name__ == "__main__":
+    host = "127.0.0.1"
+    tls_port=8443
+    max_threads = 5
+    tls_server = SocketServer(host, tls_port, max_threads,True)
+    tls_server.start()
